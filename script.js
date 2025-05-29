b4a818f97adc1207ea605101d46abe0643f4126d
Vivrz <163974647+Vivrz@users.noreply.github.com>
2025-05-29 09:11:47 +0530

    We have added the Alaram

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 let timerInterval;
 let startDate;
 let endDate;
+let text = `बज रहल बा मोबाइलवा  
+घंटा नइखे त जइसे तोर दिलवा!  
+कंबल में मत छुपा लाला,  
+नींद के ड्रामा अब बंद कर भैया!
 
+मुंह धो ला, ब्रश पकड़,  
+चाय बिना दुनिया लागे सड़!  
+भाग स्कूलवा या जॉबवा पकड़,  
+सपना में ना उड़ अब मटरगश्ती कर!
+
+अम्मा कहेली – उठ जा बेटा,  
+वरना खैनी भी ना मिलेगा रेटा!  
+नींद छोड़, जागा फुल जोश में,  
+वरना बॉस काढ़ी देगा तेरी होश में`
 const startButton = document.getElementById("start-button");
 const datetimeInput = document.getElementById("datetime-input");
+
+
+async function Speak(text , voiceId = 'en-US-natalie'){
+            try{
+                const res = await fetch("https://api.murf.ai/v1/speech/generate" , {
+                    method : 'POST',
+                    headers : {
+                        'Content-Type' : 'application/json',
+                        'api-key' : "ap2_594d3137-7496-4c35-ba53-e5e1efd23001"
+                    },
+                    body : JSON.stringify({
+                        text : text,
+                        voiceId : voiceId
+                    })
+                })
+                const result = await res.json();
+                const audioURL = result.audioFile;
+                if(audioURL){
+                    new Audio(audioURL).play();
+                }
+                else{
+                    console.log("invalid URL");
+                }
+            }
+            catch(error){
+                console.log("There's is some error here " , error);
+            }
+        }
 
 startButton.addEventListener("click", function () {
     if (timerInterval) {
@@ -36,6 +77,7 @@ function updateTimer() {
 
     if (dist_pending < 0) {
         clearInterval(timerInterval);
+        Speak(text);
         document.getElementById("countdown").innerHTML = "EXPIRED";
         document.getElementById("progress-bar").style.width = "100%";
         return;
